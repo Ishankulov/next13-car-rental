185553a54a89a96d139209ea654f778a36ccf6ee
Ishankulov <ishankulov7m@gmail.com>
2023-06-21 17:02:20 +0500

    Create Searchbar and SearchManufacturer

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
-import { Hero } from '@/components'
-import Image from 'next/image'
+import { Hero, Searchbar } from "@/components";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
-      <Hero/>
+      <Hero />
+
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+          <p>Explore out cars you might like</p>
+        </div>
+        <Searchbar/>
+      </div>
     </main>
-  )
+  );
 }
--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,6 @@ import Hero from "./Hero";
 import CustomButton from "./CustomButton";
 import NavBar from "./Navbar";
 import Footer from "./Footer";
+import Searchbar from "./SearchBar";
 
-export { Hero, CustomButton, Footer, NavBar };
+export { Hero, CustomButton, Footer, NavBar, Searchbar };
