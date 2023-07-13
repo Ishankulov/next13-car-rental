1bf8b5a22e48f85e59e5dc6056c815c1074b96c8
Ishankulov <ishankulov7m@gmail.com>
2023-07-13 17:14:12 +0500

    Create some Components

--- components/index.ts
+++ components/index.ts
@@ -1,7 +1,19 @@
-import Hero from "./Hero";
+import CarCard from "./CarCard";
 import CustomButton from "./CustomButton";
-import NavBar from "./Navbar";
+import CustomFilter from "./CustomFilter";
 import Footer from "./Footer";
-import Searchbar from "./SearchBar";
+import NavBar from "./Navbar";
+import ShowMore from "./ShowMore";
+import SearchBar from "./Searchbar";
+import Hero from "./Hero";
 
-export { Hero, CustomButton, Footer, NavBar, Searchbar };
+export {
+  Hero,
+  CarCard,
+  CustomButton,
+  CustomFilter,
+  Footer,
+  NavBar,
+  ShowMore,
+  SearchBar,
+};
